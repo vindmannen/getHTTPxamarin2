3fa4bcba107246ae229d8c8391e31fc7f6e90c1f
Dell <Dell@DESKTOP-P5SHEKE>
2021-11-10 00:07:21 +0100

    Add project files.

---
+++ getHTTPxamarin2/Kolada.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace getHTTPxamarin2
+{
+    
+
+        public class Values
+        {
+            public Kolada[] values { get; set; }
+            public int count { get; set; }
+        }
+
+        public class Kolada
+        {
+            public string kpi { get; set; }
+            public string municipality { get; set; }
+            public string period { get; set; }
+            public float value { get; set; }
+            public object value_m { get; set; }
+            public object value_f { get; set; }
+        }
+
+    
+}
---
+++ getHTTPxamarin2/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using Microcharts;
+using SkiaSharp;
+
+namespace getHTTPxamarin2
+{
+    public partial class MainPage : ContentPage
+
+    {
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            var entries = new List<ChartEntry>();
+
+            var httpClient = new HttpClient();
+            var resultJSON = await httpClient.GetStringAsync("https://api.kolada.se/v1/data/permunicipality/N07403/0180");
+            Values resultKolada = JsonConvert.DeserializeObject<Values>(resultJSON);
+                                 
+            foreach (var item in resultKolada.values)
+            {
+                Random ran = new Random();
+                SKColor randomColor = SKColor.FromHsv(ran.Next(256), ran.Next(256), ran.Next(256));
+                var entry = new ChartEntry(item.value)
+                {
+                    Label = item.value.ToString(),
+                    Color = randomColor,
+                    ValueLabel = item.period
+                };
+                entries.Add(entry);
+             }
+            chartViewBar.Chart = new BarChart
+            {
+                Entries = entries,
+                ValueLabelOrientation = Orientation.Vertical,
+                LabelTextSize = 30
+            };
+        }
+    }
+}
